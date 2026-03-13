d6a313efce99f8bb6da8aa1ced4902d0aa2c4a75
Guntur <gunturpdng@gmail.com>
2026-03-13 15:40:15 +0800

    feat: add flake direnv for golang hugo

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Hugo development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hugo
+            git
+            go 
+          ];
+
+          shellHook = ''
+            echo "--- Hugo Development Environment ---"
+            hugo version
+          '';
+        };
+      });
+}
